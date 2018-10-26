cb2b5be38a385ddaac99ddd762c74e9b54450387
swilliam01 <sehrishwilliam@gmail.com>
2018-10-25 23:25:41 -0400

    initial comment

---
+++ src/main/java/com/example/demo/TestPractice01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPractice01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPractice01Application.class, args);
+    }
+}
